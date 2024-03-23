a229fa5fdf85df8a7c54414784cf958564527590
spookeyy <pangasmeshack@gmail.com>
2024-03-23 19:24:16 +0300

    complete lab

--- index.js
+++ index.js
@@ -4,21 +4,25 @@ const num2 = 2;
 const multiply = num1 * num2;
 console.log(multiply);
 
-function random() {
-  return Math.floor(Math.random() * 100) + 1; // Generate a random integer between 1 and 100 (inclusive)
-}
-console.log(random());
+// function random() {
+//   return Math.floor(Math.random() * 100) + 1; // Generate a random integer between 1 and 100 (inclusive)
+// }
+// console.log(random());
+let random = Math.ceil(Math.random() * Number.MAX_SAFE_INTEGER);
 
-function mod(num3, num4) {
-  return num3 % num4;
-}
-console.log(mod(13, 9));
+
+
+  let num3 = 13;
+  let num4 = 9;
+  let mod = num3 % num4;
+console.log(mod);
 // Function to find the maximum value in a set of numbers
-function max() {
-  const numbers = [20, 5, 6, 4, 16]
-  if (!numbers || numbers.length === 0) {
-    return undefined; // Handle empty array or no arguments
-  }
-  return Math.max(...numbers); // Efficiently find the maximum using spread operator(...)
-}
-console.log(max());
+// function max() {
+//   const numbers = [20, 5, 6, 4, 16]
+//   return Math.max(...numbers);
+// }
+// console.log(max());
+
+let numbers = [10, 20, 5];
+let max = Math.max(...numbers)
+console.log(max)
